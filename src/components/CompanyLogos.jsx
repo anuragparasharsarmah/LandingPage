d3bd1a1bde8459651571eae81fa1174aa51a8204
anuragpsarmah <anurag.parashar@mca.christuniversity.in>
2024-05-02 21:32:05 +0530

    Improvements

--- src/components/CompanyLogos.jsx
+++ src/components/CompanyLogos.jsx
@@ -4,14 +4,14 @@ const CompanyLogos = ({ className }) => {
   return (
     <div className={className}>
       <h5 className="tagline mb-6 text-center text-n-1/50">
-        Created by
+        Developed by
       </h5>
       <ul className="flex">
         <div className="flex items-center justify-center flex-1 h-[8.5rem] space-x-6">
           {/* Adjusted to wrap each name in a <div> */}
-          <div className="name-item flex-1 text-center">Adarsh</div>
-          <div className="name-item flex-1 text-center">Anurag</div>
-          <div className="name-item flex-1 text-center">Chiranjeev</div>
+          <div className="name-item flex-1 text-center">Adarsh Kumar Dash</div>
+          <div className="name-item flex-1 text-center">Anurag Parashar Sarmah</div>
+          <div className="name-item flex-1 text-center">Chiranjeev Sehgal</div>
         </div>
       </ul>
     </div>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,9 +20,9 @@ const Hero = () => {
       <div className="container relative" ref={parallaxRef}>
         <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[3.875rem] md:mb-20 lg:mb-[6.25rem]">
           <h1 className="h1 mb-6">
-            Explore the Possibilities of&nbsp;AI&nbsp;Chatting with {` `}
+          Unlock Your Success Story in Tech with {` `}
             <span className="inline-block relative">
-              Brainwave{" "}
+              OnClique{" "}
               <img
                 src={curve}
                 className="absolute top-full left-0 w-full xl:-mt-2"
@@ -33,8 +33,7 @@ const Hero = () => {
             </span>
           </h1>
           <p className="body-1 max-w-3xl mx-auto mb-6 text-n-2 lg:mb-8">
-            Unleash the power of AI within Brainwave. Upgrade your productivity
-            with Brainwave, the open AI chat app.
+          Reach new heights through personalized training. Elevate your coding skills with OnClique.
           </p>
           <Button white onClick={handleGetStartedClick}>
             Get started
